d48dfdc99b9e7d811a145fe6e5427a778afdff67
AldiyarMaget <241527@astanait.edu.kz>
2025-01-30 11:06:41 +0500

    aidos sochni

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import controllers.HotelController;
+import controllers.RoomController;
+import controllers.BookingController;
+import controllers.interfaces.IHotelController;
+import controllers.interfaces.IRoomController;
+import controllers.interfaces.IBookingController;
+import data.PostgresDB;
+import data.interfaceces.IDB;
+import repositories.HotelRepository;
+import repositories.RoomRepository;
+import repositories.BookingRepository;
+import repositories.interfaces.IHotelRepository;
+import repositories.interfaces.IRoomRepository;
+import repositories.interfaces.IBookingRepository;
+
 public class Main {
     public static void main(String[] args) {
+        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "postgres", "0000", "hotel_booking");
 
+        IHotelRepository hotelRepo = new HotelRepository(db);
+        IRoomRepository roomRepo = new RoomRepository(db);
+        IBookingRepository bookingRepo = new BookingRepository(db);
+
+        IHotelController hotelController = new HotelController(hotelRepo);
+        IRoomController roomController = new RoomController(roomRepo);
+        IBookingController bookingController = new BookingController(bookingRepo);
+
+        HotelBookingApplication app = new HotelBookingApplication(hotelController, roomController, bookingController);
+        app.mainMenu();
+
+        db.close();
     }
 }
--- src/controllers/HotelController.java
+++ src/controllers/HotelController.java
@@ -1,4 +1,19 @@
 package controllers;
 
-public class HotelController {
+import controllers.interfaces.IHotelController;
+import repositories.interfaces.IHotelRepository;
+import models.Hotel;
+import java.util.List;
+
+public class HotelController implements IHotelController {
+    private final IHotelRepository hotelRepository;
+
+    public HotelController(IHotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    }
+
+    @Override
+    public List<Hotel> getHotelsByCity(String city) {
+        return hotelRepository.getHotelsByCity(city);
+    }
 }
